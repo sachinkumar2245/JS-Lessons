ca405da8af43a4e957d19681971761f1ee98fcf1
sachinkumar2245 <96250003+sachinkumar2245@users.noreply.github.com>
2024-10-24 17:57:56 +0000

    new commit

--- basics/01_variables.js
+++ basics/01_variables.js
@@ -2,3 +2,8 @@ const account_Id= 12345;
 let account_email= "sachinkumm@gmail.com";
 var account_password= "80900";
 account_city="jaipur";
+
+// account_Id=2;
+
+console.log(account_Id);
+console.table([account_Id, account_email, account_password, account_city]);
