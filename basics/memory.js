d089f0b1a761a42fbdb3bb8130d356d778b2a993
sachinkumar2245 <96250003+sachinkumar2245@users.noreply.github.com>
2024-10-25 13:18:52 +0000

    memory commit

---
+++ basics/memory.js
@@ -0,0 +1,18 @@
+// stack(primitice) and heap(non-primitive);
+let myYoututbename="Sachin_Kumar";
+let anotherName= myYoututbename;
+anotherName="chai aur code";
+
+console.log(myYoututbename);
+console.log(anotherName);
+
+let user0ne={
+    email: "sachin@sachin.com",
+    upi: "sachina@okhdfcbank"
+}
+
+let userTwo=user0ne; 
+userTwo.email = "hitesh@gmail.com";
+console.log(user0ne);
+console.log(userTwo.email);
+
