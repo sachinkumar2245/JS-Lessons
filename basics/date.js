903936894b7c478df60bb6e9ddb4e796e54529a2
sachinkumar2245 <96250003+sachinkumar2245@users.noreply.github.com>
2024-10-25 14:15:32 +0000

    date commit

---
+++ basics/date.js
@@ -0,0 +1,10 @@
+// dates;
+
+let mydate= new Date();
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+
+let myCreateDate = new Date();
+
