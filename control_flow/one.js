211cd5928f61258c50a4914380ab6a9780d26969
sachinkumar2245 <96250003+sachinkumar2245@users.noreply.github.com>
2024-11-15 12:53:32 +0000

    truthy

--- control_flow/one.js
+++ control_flow/one.js
@@ -2,11 +2,56 @@
 
 const a = 44;
 const b= 55;
-let total;
-if (a<b){
-    total = a+b;
+let total;  // operators --> <,>, <=, >=, !=, ===, !==
+if (2==="2"){ // double equal to checks for the value comparision and 
+    total = a+b; //triple equal to sign checks for the value as well as type comparision
     
 }
-console.log(total); 
+// console.log(total); 
+
+// const score = 200;
+
+// if (score>50) {
+//     const pwr = "fly";
+//     console.log(`Use power: ${pwr}`);
+    
+// };
+
+
+// const balance =1000;
+
+// if (balance>500) console.log("test"), console.log("test2");  // not a good practice 
+
+// if (balance<500) {
+//     console.log("less than 500");
+    
+// } else if(balance<900){
+//     console.log("less than 900");
+    
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInfromGoogle=false;
+const loggedInFromEmail = true;
+
+
+if (userLoggedIn && debitCard ) {
+    console.log("allow to buy course");
+    
+}
+ 
+if(loggedInfromGoogle || loggedInFromEmail){
+    console.log("user logged in");
+    
+}
+
+
+
+
+
+
+
+
 
 
---
+++ control_flow/truthy.js
@@ -0,0 +1,17 @@
+const userEmail =""
+
+if (userEmail) {
+    console.log("got user email");
+    
+} else{
+    console.log("dont have user email");
+    
+}
+
+// falsy values
+// false , 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+//"0", 'false', " ", [], {}, function(){}
+
+if 
