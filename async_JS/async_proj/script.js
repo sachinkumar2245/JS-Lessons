65a82bf9ec43127ff3d295880876535fa38d8158
sachinkumar2245 <96250003+sachinkumar2245@users.noreply.github.com>
2024-11-29 12:42:16 +0000

    eveee

--- async_JS/async_proj/script.js
+++ async_JS/async_proj/script.js
@@ -1,6 +1,30 @@
 // generate a random color 
 
 const randomColor = function(){
-    const hex ='';
+    const hex ='0123456789ABCDEF';
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random()*16)];
+         
+        
+    }
+    return color;
+
+
     
+};
+
+const startChangingColor= ()=>{
+    setInterval(changeBgColor, 1000);
+
+    changeBgColor()=>{
+
+    }
 }
+const stopChangingColor= ()=>{
+
+}
+
+document.querySelector("#start").addEventListener('click', startChangingColor);
+document.querySelector("#stop").addEventListener('click', stopChangingColor);
+
